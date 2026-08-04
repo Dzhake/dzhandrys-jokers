751b857dab5b38f8b04475487f402a2c331dec19
Dzhake <andryzindmi@gmail.com>
2026-08-05 00:13:35 +0300

    fix p03 not using events

--- items/jokers/inscryption/p03.lua
+++ items/jokers/inscryption/p03.lua
@@ -25,25 +25,38 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.blueprint then return end
+
         if context.setting_blind then
-            --event
             local size_diff = card.ability.extra.hand_size - card.ability.extra.hand_size_default
-            G.hand:change_size(-size_diff)
-            card.ability.extra.hand_size = card.ability.extra.hand_size_default
             return {
                 message = localize("k_reset"),
                 colour = G.C.FILTER,
+                func = function()
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            G.hand:change_size(-size_diff)
+                            card.ability.extra.hand_size = card.ability.extra.hand_size_default
+                            return true
+                        end
+                    }))
+                end
             }
         end
 
         if context.after or context.pre_discard then
             local size_diff = card.ability.extra.hand_size_mod
-            --event
-            G.hand:change_size(size_diff)
-            card.ability.extra.hand_size = card.ability.extra.hand_size + size_diff
             return {
                 message = localize { type = "variable", key = "a_chips", vars = { size_diff } },
                 colour = G.C.FILTER,
+                func = function()
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            G.hand:change_size(size_diff)
+                            card.ability.extra.hand_size = card.ability.extra.hand_size + size_diff
+                            return true
+                        end
+                    }))
+                end
             }
         end
     end,
