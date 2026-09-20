f95e99cbb5dacf687663dba3c0ed7bae6f23eadd
Dzhake <andryzindmi@gmail.com>
2026-09-21 00:09:55 +0300

    add release script

---
+++ release.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+rem echo "start"
+rem Config:
+set	dirsToCopy=assets\ items\ localization\ lovely\ main\
+set filesToCopy=config.lua main.lua metadata.json LICENSE
+rem End of config!
+set buildDir=%~dp0Build\\
+rem echo "variables created"
+(for %%d in (%dirsToCopy%) do ( 
+   xcopy %~dp0%%d %buildDir%%%d /Y /E /H /Q
+))
+rem echo "folders copied"
+(for %%f in (%filesToCopy%) do ( 
+   xcopy %~dp0%%f %buildDir%%%f* /Y /H /Q
+))
+rem echo "files copied"
+7z a %~dp0dzhandrys-jokers.zip %buildDir%/*
+rem echo "zipped"
+rd /s /q "%builddir%"
+rem echo "finished"
